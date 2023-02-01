e8378a55d576aa8fb24cbc6d5390411c454c426e
jain <janepark423@gmail.com>
2023-02-02 03:00:38 +0900

    database 값 screen에 보이기

--- startpy_lecture/views.py
+++ startpy_lecture/views.py
@@ -5,4 +5,8 @@ from .models import myText
 
 def home_list(request) :
     texts = myText.objects.filter()
+
+    # texts1 = "1번 타이틀"
+    # texts2 = "2번 타이틀"
+
     return render(request, 'startpy_lecture/home_list.html', {'texts1': texts})
