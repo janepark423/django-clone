8274d122465b8f06a21643638af6ab6b8833257e
jain <janepark423@gmail.com>
2023-02-02 02:30:22 +0900

    모델부 작성

--- startpy_lecture/models.py
+++ startpy_lecture/models.py
@@ -1,5 +1,10 @@
+from django.conf import settings
 from django.db import models
+
 
 # Create your models here.
 class myText(models.Model) :
-    author = models.ForeignKey
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    contents = models.CharField(max_length=200)
+    img_url = models.CharField(max_length=200)
