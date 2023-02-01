8c52528813df9e38f8d9f28b25852e705f23c4c0
jain <janepark423@gmail.com>
2023-02-02 02:57:57 +0900

    database 값 screen에 보이기

--- startpy_lecture/views.py
+++ startpy_lecture/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import myText
 
 # Create your views here.
 
 def home_list(request) :
-    return render(request, 'startpy_lecture/home_list.html')
+    texts = myText.objects.filter()
+    return render(request, 'startpy_lecture/home_list.html', {'texts1': texts})
